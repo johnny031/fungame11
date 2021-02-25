e2f8b918bd674acd969474d4ed16136340e6e3fc
Johnny <johnny055238@gmail.com>
2021-02-26 03:30:56 +0800

    first

---
+++ main.js
@@ -0,0 +1,28 @@
+let words = [["奶茶","咖啡"],["梅花","黑桃"],["貝多芬","莫札特"],["麻糬","湯圓"],["游泳","浮潛"]]
+let count = 0;
+let surprise = false;
+
+$("#next_button").on("click", function(){
+    if(!surprise){
+        $("#answer_h1").html(words[count][0]);
+        $("#trap_h1").html(words[count][1]);
+        count++;
+    } else {
+        alert("請點擊繼續");
+        alert("請點擊繼續");
+        alert("若為出題者請繼續...");
+        alert("謝謝你的...");
+        alert("諸多照顧...");
+        alert("我...");
+        alert("只是想說...");
+        setTimeout(() => {
+            $("body").css({"background-color": "pink"});
+        }, 800);
+        $(".board").fadeOut(800);
+        $(".background").fadeOut(800);
+        $("#birthday_div").delay(800).fadeIn(800);
+    }
+});
+$("#surprise_button").on("click", function(){
+    surprise = true;
+});
